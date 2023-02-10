7e94de38e59aa08746b80edef1b54131af62feeb
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-02-10 09:57:08 -0300

    User api e expo-application

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,3 +8,12 @@ export const kanguruApi = axios.create({
       "X-Parse-REST-API-Key": "uEtFO1gKixJvpj1tVVMnjr6jI7jJxxqJHd18w7n4",
    },
 })
+
+export const userApi = axios.create({
+   baseURL: "https://parseapi.back4app.com/users",
+   headers: {
+      Accept: "application/json",
+      "X-Parse-Application-Id": "J4VmpvYgyyMLIKv6Taw091kaYzt5TqswJHnAgJ9g",
+      "X-Parse-REST-API-Key": "uEtFO1gKixJvpj1tVVMnjr6jI7jJxxqJHd18w7n4",
+   },
+})
