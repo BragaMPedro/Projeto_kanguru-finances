ce3f905f8cf939e3a67c429a243c192548a8efa6
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-02-11 12:15:49 -0300

    refactor: Login Routes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,20 +1,20 @@
 import React from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { Login } from "../screens/Login/Login"
+import { LoginScreen } from "../screens/Login/index"
 import { Home } from "../screens/Home/Home"
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export type RootStackParamList = {
-    Login: undefined
+   LoginScreen: undefined
     Home: undefined
  }
 
  const StackNavigator = () => {
     return (
        <Stack.Navigator screenOptions={{ headerShown: false }} >
-          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="Home" component={Home} options={{animation: 'slide_from_right'}} />
        </Stack.Navigator>
     )
