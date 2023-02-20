dee9e0a5274e4279c6a4390b1e9f6c67866848e3
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-02-20 09:42:36 -0300

    feat: Home inicial & Calendar

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,24 +1,62 @@
 import { useState, useEffect } from "react"
-import { FunctionsHome } from './functions'
+import { FunctionsHome } from "./functions"
 import { Text, View, TouchableOpacity, Alert, FlatList } from "react-native"
 
 import { styles } from "./styles"
 import { PagamentoProps } from "../../services/PagamentosService"
+import { Calendar, CalendarList, LocaleConfig } from "react-native-calendars"
 
 export const Home = () => {
    const [pagamentoArray, setPagamentoArray] = useState<PagamentoProps[]>([])
- 
-   const { pegarPagamentos, } = FunctionsHome({pagamentoArray, setPagamentoArray})
+
+   const { pegarPagamentos } = FunctionsHome({ pagamentoArray, setPagamentoArray })
 
    // useEffect(()=>{
    //    pegarPagamentos()
    // },[])
 
+   LocaleConfig.locales["pt-BR"] = {
+      monthNames: [
+         "Janeiro",
+         "Fevereiro",
+         "Março",
+         "Abril",
+         "Maio",
+         "Junho",
+         "Julho",
+         "Agosto",
+         "Setembro",
+         "Outubro",
+         "Novembro",
+         "Dezembro",
+      ],
+      monthNamesShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul.", "Ago", "Set.", "Out.", "Nov.", "Dec."],
+      dayNames: ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"],
+      dayNamesShort: ["Dom.", "Seg.", "Ter.", "Qua.", "Qui.", "Sex.", "Sáb."],
+      today: "Hoje",
+   }
+   LocaleConfig.defaultLocale = "pt-BR"
+
+
    return (
       <View style={styles.container}>
-         <Text style={{ color: "#fff", textAlign: "center", fontSize: 14, fontWeight: "bold" }}>
-            Página Home maneirissima aqui
-         </Text>
+         {/* <View style={styles.calendarContainer}> */}
+         <Calendar
+            minDate={ `2023-${new Date().getMonth()}-01`}
+            maxDate={ `2023-${new Date().getMonth() + 2}-01`}
+            hideExtraDays
+            displayLoadingIndicator={false}
+            style={styles.calendar}
+            calendarWidth={300}
+            firstDay={7}
+            monthFormat={"dd MMMM"}
+         />
+         {/* </View> */}
+         <View style={styles.content}>
+            <Text style={{ color: "#fff", textAlign: "center", fontSize: 14, fontWeight: "bold" }}>
+               Página Home maneirissima aqui
+            </Text>
+         </View>
       </View>
    )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,24 +1,32 @@
 import { StyleSheet, Dimensions, StatusBar } from "react-native"
 
-const {height, width} = Dimensions.get('window');
+const { height, width } = Dimensions.get("window")
 
 export const styles = StyleSheet.create({
-
-container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingTop: StatusBar.currentHeight,
-    padding: 16,
-    backgroundColor: "#3E3C42",
- },
- buttonAutenticado: {
-    height: 50,
-    width: 252,
-    backgroundColor: '#fa7828',
-    borderRadius: 50,
-    justifyContent: "center",
-    marginVertical: 8,
- },
+   container: {
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: "space-between",
+      alignItems: "center",
+      paddingTop: StatusBar.currentHeight + 5,
+      backgroundColor: "#3E3C42",
+   },
+   calendarContainer: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+   },
+   calendar:{
+      width: 314,
+      borderRadius: 10,
+   },
+   content: {
+      flex: 1.37,
+      borderTopRightRadius: 30,
+      borderTopLeftRadius: 30,
+      alignItems: "center",
+      justifyContent: "center",
+      width: '100%',
+      elevation: 3,
+   },
 })
